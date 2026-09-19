8c2e5a6416eccde10f981221ba7bae01d99dfbc5
Abdul-Samad-dotcom <abdoabdomdia@gmail.com>
2026-09-19 16:34:59 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js — يسمح للتطبيق بالعمل دون اتصال بالإنترنت بعد أول زيارة
-const CACHE_NAME = 'ajrati-cache-v3';
+const CACHE_NAME = 'ajrati-cache-v4';
 const APP_SHELL = [
   './',
   './index.html',
